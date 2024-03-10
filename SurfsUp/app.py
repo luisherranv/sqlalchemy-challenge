5962770ff1f78a6150705f64a745fd495bea4f62
luisherranv <luisherranv@gmail.com>
2024-03-10 13:57:59 -0400

    commit

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,32 +1,54 @@
 # Import the dependencies.
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect, func
+
+from flask import Flask, jsonify
 
 
+# # Database Setup
+# #################################################
 
-#################################################
-# Database Setup
-#################################################
-
-# Create engine using the `hawaii.sqlite` database file
-
-# Declare a Base using `automap_base()`
-
-# Use the Base class to reflect the database tables
-
+# # Create engine using the `hawaii.sqlite` database filecle
+engine = create_engine("sqlite:///hawaii.sqlite")
+# engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# # Declare a Base using `automap_base()`
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
 
+Measurement = Base.classes.measurement
+# Station = Base.classes.station
 
-# Create a session
+# # Create a session
+# session = Session(engine)
+
+# #################################################
+# # Flask Setup
+# #################################################
+# app = Flask(__name__)
 
 
-#################################################
-# Flask Setup
-#################################################
 
+# #################################################
+# # Flask Routes
+# #################################################
+# @app.route("/")
+# def home():
+#     return (
+#         f"Welcome to the Hawaii Climate App<br/>"
+#         f"ALOHA!!<br/>"
+#         f" <br/>"
+#         f"Available Routes: <br/>"
+#         f"/api/v1.0/precipitation<br/>"
+#         f"/api/v1.0/stations<br/>"
+#         f"/api/v1.0/tobs<br/>"
+#         f"<br/>"
+#     )
 
-
-
-#################################################
-# Flask Routes
-#################################################
+# if __name__ == "__main__":
+#     app.run(debug=True)
